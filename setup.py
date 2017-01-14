1e7ead231dff7d3b1fb9266b9941a980d87e71ea
Luigi Mori <lmori@paloaltonetworks.com>
2017-01-14 03:49:19 +0000

    bump to version 0.2 with move to MM external extension
    Signed-off-by: Luigi Mori <lmori@paloaltonetworks.com>

--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
 from setuptools import Extension, setup, find_packages
 
 import sys
+import json
 import os.path
 sys.path.insert(0, os.path.abspath('.'))
-
-from youtubeminer import __version__
 
 with open('requirements.txt') as f:
     _requirements = f.read().splitlines()
 
+with open('minemeld.json') as f:
+    _metadata = json.load(f)
+
+_entry_points={}
+if 'entry_points' in _metadata:
+    for epgroup, epoints in _metadata['entry_points'].iteritems():
+        _entry_points[epgroup] = ['{} = {}'.format(k, v) for k, v in epoints.iteritems()]
+
 setup(
-    name='youtube-miner',
-    version=__version__,
-    author='Palo Alto Networks',
-    author_email='techbizdev@paloaltonetworks.com',
-    description='Sample MineMeld node extension for mining Youtube API',
+    name=_metadata['name'],
+    version=_metadata['version'],
+    author=_metadata['author'],
+    author_email=_metadata['author_email'],
+    description=_metadata['description'],
     classifiers=[
+        'Framework :: MineMeld',
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2.7',
@@ -28,12 +36,5 @@ setup(
     package_data = {
         '': ['prototypes/*.yml']
     },
-    entry_points={
-        'minemeld_nodes': [
-            'youtubeminer.Miner = youtubeminer.node:Miner'
-        ],
-        'minemeld_prototypes': [
-            'youtubeminer.prototypes = youtubeminer:prototypes'
-        ]
-    }
+    entry_points=_entry_points
 )
--- youtubeminer/__init__.py
+++ youtubeminer/__init__.py
@@ -1,5 +1,3 @@
-__version__ = '0.1'
-
 def prototypes():
     import os
 
